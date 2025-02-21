70176a42f00cf7552ed4858d0570487a48f6b43b
sergJess <serg.fantasy@yandex.ru>
2025-02-21 21:25:28 +0300

    feat: add styles for header and nav

--- src/components/navigation/nav.tsx
+++ src/components/navigation/nav.tsx
@@ -3,8 +3,11 @@ import { NavLink } from "react-router";
 import * as styles from './style.module.scss';
 export const Nav = () => {
     return <nav className={styles.nav}>
-        <NavLink to='/' end>Home</NavLink>
-        <NavLink to='/game' end>Game</NavLink>
-        <NavLink to='/settings'>Settings</NavLink>
+        <NavLink className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.linkActive}` : `${styles.link}`} to='/' end>Home</NavLink>
+        <NavLink className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.linkActive}` : `${styles.link}`} to='/game' end>Game</NavLink>
+        <NavLink className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.linkActive}` : `${styles.link}`} to='/settings'>Settings</NavLink>
     </nav>
 }

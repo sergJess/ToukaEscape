964314cfc222f85f2c1d22229ee9c7b128252683
sergJess <serg.fantasy@yandex.ru>
2025-02-25 21:54:39 +0300

    feat: add images of main character of the game

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import * as styles from './style/style.module.scss';
+import characterImg from '../../assets/character/toukaYoko99.jpg';
 export const HomePage = () => {
-    return <div className={styles.page}><h2 className={styles.title}>Home</h2></div>
+    const title = 'Touka YokoHame [Yoko] E[sc]apeX';
+    return <div className={styles.page}><h2 className={styles.title}>{title}</h2>
+        <div><img alt='image of hope' src={characterImg}></img></div>
+    </div>
 }

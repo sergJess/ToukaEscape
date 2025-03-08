9a96a384735c1c7dac14033e46151f6cb233481b
sergJess <serg.fantasy@yandex.ru>
2025-03-08 21:15:46 +0300

    feat: add start button goes up when is clicked

--- src/pages/game/game.tsx
+++ src/pages/game/game.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import * as styles from './style/style.module.scss';
 export const Game = () => {
-    return <div className={styles.gamePage}><button className={styles.startButton}>Start the GAME[X]</button></div>
+    const refToStartButton = useRef<HTMLButtonElement | null>(null);
+    const clickOnButton = () => {
+        const button = refToStartButton.current;
+        if (button) {
+            button.classList.add(`${styles.startButtonClicked}`);
+        }
+        console.log(12);
+    };
+    return <div className={styles.gamePage}><button ref={refToStartButton} onClick={clickOnButton} className={styles.startButton}>Start the GAME[X]</button></div>
 }

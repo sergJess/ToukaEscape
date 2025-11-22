cc2d3d3b1367d3ebef83be4a6cc520bc8fd1d838
sergJess <serg.fantasy@yandex.ru>
2025-11-22 22:23:33 +0300

    update repo

--- src/game/config/config.ts
+++ src/game/config/config.ts
@@ -1,9 +1,12 @@
 import 'phaser';
+import { Scene } from 'phaser';
+import toukaMotoPoster from '../../assets/character/ToukaYoko00.jpeg';
 export const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: 856,
     height: 824,
     zoom: 2,
+
     input: {
         keyboard: true,
         gamepad: true,
@@ -23,5 +26,28 @@ export const config: Phaser.Types.Core.GameConfig = {
             },
         },
     },
-    // scene: [],
+    scene: {
+        preload: () => { },
+
+    }
 }
+class Game {
+    game: Phaser.Game;
+    constructor(config: Phaser.Types.Core.GameConfig) {
+        this.game = new Phaser.Game({ ...config, scene: { preload: this.preload } });
+    }
+    preload() {
+    }
+
+}
+export class Preloader extends Scene {
+    constructor() {
+        super('Preloader');
+    }
+    preload() {
+        this.load.image('poster', `${toukaMotoPoster}`);
+    }
+}
+export const game = new Game(config);
+
+
---
+++ src/game/scene/basic-functions/preload.ts
@@ -0,0 +1,6 @@
+import 'phaser';
+// import toukaMotoPoster from '../../../assets/character/ToukaYoko00.jpeg';
+export function preload() {
+    // const game: Phaser.Game;
+    // game.load.image('poster', `${toukaMotoPoster}`);
+}
